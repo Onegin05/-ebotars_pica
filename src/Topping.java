e6bad6a4f0b7e96f5a508a316649f2730abbd147
Cebotars <gikon603@gmail.com>
2023-03-16 11:25:24 +0200

    logs papildīnajumi pīlnigi gatavs

--- src/Topping.java
+++ src/Topping.java
@@ -1,4 +1,6 @@
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -6,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Topping extends JFrame {
+public  class Topping extends JFrame implements ActionListener {
 		JButton bB = new JButton("Becons");
 		JButton pB = new JButton("Peperoni");
 		JButton sB = new JButton("Extra siers");
@@ -23,19 +25,19 @@ public class Topping extends JFrame {
 			panel.add(topName);
 			add(panel);		
 			
-			JLabel becons = new JLabel(new ImageIcon(""));
+			JLabel becons = new JLabel(new ImageIcon("Becons.jpg"));
 			panel.add(becons);
 			becons.setBounds(50,70,300,300);
 			
-			JLabel peperoni = new JLabel(new ImageIcon(""));
+			JLabel peperoni = new JLabel(new ImageIcon("Peperoni.jpg"));
 			panel.add(peperoni);
 			peperoni.setBounds(350,70,300,300);
 			
-			JLabel siers = new JLabel(new ImageIcon(""));
+			JLabel siers = new JLabel(new ImageIcon("Extra siers.jpg"));
 			panel.add(siers);
 			siers.setBounds(50,400,300,300);
 			
-			JLabel sipols = new JLabel(new ImageIcon(""));
+			JLabel sipols = new JLabel(new ImageIcon("Sîpols.jpg"));
 			panel.add(sipols);
 			sipols.setBounds(350,400,300,300);
 			
@@ -58,6 +60,12 @@ public class Topping extends JFrame {
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			setSize(garums,platums);
 			setLocation(1920/2 - garums/2, 1080/2 - platums/2);
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
 		}
 }
 
